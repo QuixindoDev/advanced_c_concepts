7bd83628eb4a1e8655bd0d21e744b6f68802447b
QuixindoDev <eufrasio.dev@gmail.com>
2024-10-12 21:24:22 +0100

    first commit

---
+++ fork.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  fork.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  12/10/2024 21:19:36
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Eufrásio QUixindo (), eufrasio.dev@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main()
+{
+
+}
